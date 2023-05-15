bf152534d192848d014f7fafa71e25d2c3f46b9a
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-05-15 11:56:44 -0700

    Fix imports

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import Auth from '../../utils/auth';
+import Auth from '../utils/auth';
 import { useMutation } from '@apollo/client';
-import { ADD_USER, LOGIN_USER } from '../../utils/mutations';
+import { ADD_USER, LOGIN_USER } from '../utils/mutations';
 
 const Signup = ({ changeAuthMode }) => {
   const [dataSignUp, setDataSignUp] = useState({
@@ -93,51 +93,6 @@ const Signup = ({ changeAuthMode }) => {
   );
 
 
-return (
-  <div className="Auth-form-container">
-    <form className="Auth-form" onSubmit={handleFormSubmit}>
-      <div className="Auth-form-content">
-        <h3 className="Auth-form-title">Sign In</h3>
-        <div className="text-center">
-          Not registered yet?{" "}
-          <span className="link-primary" onClick={changeAuthMode}>
-            Sign Up
-          </span>
-        </div>
-        <div className="form-group mt-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            name="email"
-            className="form-control mt-1"
-            placeholder="Enter email"
-            value={loginState.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group mt-3">
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            className="form-control mt-1"
-            placeholder="Enter password"
-            value={loginState.password}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="d-grid gap-2 mt-3">
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </div>
-        <p className="text-center mt-2">
-          Forgot <a href="#">password?</a>
-        </p>
-      </div>
-    </form>
-  </div>
-);
 return (
   <div className="Auth-form-container">
     <form className="Auth-form" onSubmit={handleFormSubmit}>
