7e35efd2c3b297e0e366d5bb00a05d97e728882b
ctvb <deenicks@hotmail.com>
2023-05-16 17:44:47 -0500

    dashboard updated

--- client/src/components/dashboard/Project/Project.js
+++ client/src/components/dashboard/Project/Project.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Project.css';
 
-const Project = ({ title, imageUrl, deployedUrl, repoUrl }) => {
+const Project = ({ title, imageUrl }) => {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(true);
+    // Perform any additional logic or navigation here
+  };
+
   return (
     <div className="project">
-      <a href={deployedUrl} target="_blank" rel="noreferrer">
-        <div className="secondary">
-          <div className="project-title">
-            <h3>{title}</h3>
-          </div>
-          <img className="project-image" src={imageUrl} alt={title} />
+      <div className="secondary" onClick={handleClick}>
+        <div className="project-title">
+          <h3>{title}</h3>
         </div>
-      </a>
+        <img className="project-image" src={imageUrl} alt={title} />
+      </div>
     </div>
   );
 };
