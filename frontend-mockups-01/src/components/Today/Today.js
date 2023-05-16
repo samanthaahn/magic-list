08a33ee0cd3cbdaa4e7e20c4793ec9dbfb25a708
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-05-15 19:01:49 -0700

    Updated Today

--- frontend-mockups-01/src/components/Today/Today.js
+++ frontend-mockups-01/src/components/Today/Today.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import Habit from '../Habit/Habit';
-// 
+import Navigation from '../Navigation/Navigation';
 import prj1 from '../../images/today.jpg';
 import prj2 from '../../images/week.jpg';
 import prj3 from '../../images/month.jpg';
 import prj4 from '../../images/history.jpg';
 import './Today.css'; // import the CSS file
 
-const Today = () => {
+const Today  = () => {
   // Actual projects data
-  // Add my projects here:
+  // Add my prtojects here:
   const habits = [
-    { title: 'Today', imageUrl: prj1, deployedUrl: 'https://samanthaahn.github.io/fiesta-event/', repoUrl: 'https://github.com/samanthaahn/fiesta-event'},
-    { title: 'This Week', imageUrl: prj2, deployedUrl: 'https://lit-ocean-94336.herokuapp.com/logins', repoUrl: 'https://github.com/jjjgm/gg-get-icecream.git' },
-    { title: 'This Month', imageUrl: prj3, deployedUrl: 'https://www.google.com', repoUrl: 'https://github.com/samanthaahn/magic-list' },
-    { title: 'History', imageUrl: prj4, deployedUrl: 'https://drive.google.com/file/d/1GnBkXZxLF4mGEsP-BnqUTsCODmaGvqWE/view?usp=share_link', repoUrl: 'https://github.com/JasperJackalope/ecommerce_back_end' },
+    { title: 'Excerise', imageUrl: prj1},
+    { title: 'Mental', imageUrl: prj2},
+    { title: 'Nutrition', imageUrl: prj3},
+    { title: 'Fun', imageUrl: prj4},
   ];
 
   return (
-    <div className="mainproject">
+    <div>
+      <Navigation />
       <section className="portfolio">
-        <h2>TODAY'S DATE</h2>
         <div className="projects-container">
           {habits.map((habit, index) => (
-            <div key={index} className="project-item">
-              <Habit {...habit} />
-            </div>
+            <Habit key={index} {...habit} />
           ))}
         </div>
       </section>
