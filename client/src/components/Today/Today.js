b89215d2c9d439d238e4c9817a1f9b31901e2c8e
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-05-16 14:04:31 -0700

    merge from main

--- client/src/components/Today/Today.js
+++ client/src/components/Today/Today.js
@@ -9,14 +9,15 @@ const Today  = () => {
     { id: 1, title: 'Habit 1' },
     { id: 2, title: 'Habit 2' },
     { id: 3, title: 'Habit 3' },
-    { id: 3, title: 'Habit 3' }
+    { id: 4, title: 'Habit 4' }
   ];
 
   return (
     <div>
       <Navigation />
-      <section className="portfolio">
-        <div className="projects-container">
+      <section className="today-main">
+        <div className="today-container">
+          {/* <h2>What are your goals today?</h2> */}
           {habits.map((habit, index) => (
             <Habit key={index} {...habit} />
           ))}
