34c6b6b700d748876df9786ee2f42534cb4f9b89
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-05-16 18:19:09 -0700

    Added Drop Down Options

--- client/src/components/Habit/Habit.js
+++ client/src/components/Habit/Habit.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import "./Habit.css"
 
-function Habit ({ title }) {
+function Habit ({ title, options }) {
   return (
     <div className="habit-container">
       {/* Text */}
-      <h2 classname="habit-title">{title}</h2>
+      <h2 className="habit-title">{title}</h2>
       {/* Dropdown */}
       <div className="dropdown">
         <select className="dropdown-select">
           <option value="">Select an option</option>
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
+          {options.map((option, index) => (
+            <option key={index} value={option}>{option}</option>
+          ))}
         </select>
       </div>
 
--- client/src/components/Today/Today.js
+++ client/src/components/Today/Today.js
@@ -6,10 +6,10 @@ import "./Today.css";
 const Today  = () => {
 
   const habits = [
-    { id: 1, title: 'Mental' },
-    { id: 2, title: 'Exercise' },
-    { id: 3, title: 'Nutrition' },
-    { id: 4, title: 'Entertainment' }
+    { id: 1, title: 'Mental', options: ['Meditate', 'Say Graditudes', 'Practice New Skill', 'Other'] },
+    { id: 2, title: 'Exercise', options: ['Take A Walk', 'Go to the Gym', 'Dance', 'Other'] },
+    { id: 3, title: 'Nutrition', options: ['Drink Water', 'Eat Your Veggies', 'Treat Yourself', 'Other'] },
+    { id: 4, title: 'Entertainment', options: ['Read A Chapter', 'Watch A New Movie', 'Listen to a Podcast', 'Other'] }
   ];
 
   return (
@@ -18,7 +18,7 @@ const Today  = () => {
       <section className="today-main">
         <div className="today-container">
           {habits.map((habit, index) => (
-            <Habit key={index} {...habit} />
+            <Habit key={index} title={habit.title} options={habit.options} />
           ))}
         </div>
       </section>
