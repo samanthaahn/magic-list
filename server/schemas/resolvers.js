9d4a25fb9cf3b85788470b31fc6c69d24e6c8ba5
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-05-17 15:06:23 -0700

    Before Merge

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -19,7 +19,6 @@ const resolvers = {
     },
     me: async (parent, args, context) => {
       if (context.user) {
-        console.log(context.user);
         return User.findOne({ _id: context.user._id }).populate("habits");
       }
       throw new AuthenticationError("You need to be logged in!");
