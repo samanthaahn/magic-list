47556f28a612588f22fd2192beeea4a2e31f7f46
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-05-18 11:38:48 -0700

    Intital DB Routes for Today

--- client/src/components/Today/Today.js
+++ client/src/components/Today/Today.js
@@ -1,16 +1,51 @@
+// import React from 'react';
+// import Habit from '../Habit/Habit';
+// import Navigation from '../Navigation/Navigation';
+// import "./Today.css";
+
+// const Today  = () => {
+
+//   const habits = [
+//     { id: 1, title: 'Mental', options: ['Meditate', 'Say Graditudes', 'Practice New Skill', 'Other'] },
+//     { id: 2, title: 'Exercise', options: ['Take A Walk', 'Go to the Gym', 'Dance', 'Other'] },
+//     { id: 3, title: 'Nutrition', options: ['Drink Water', 'Eat Your Veggies', 'Treat Yourself', 'Other'] },
+//     { id: 4, title: 'Entertainment', options: ['Read A Chapter', 'Watch A New Movie', 'Listen to a Podcast', 'Other'] }
+//   ];
+
+//   return (
+//     <div>
+//       <Navigation />
+//       <section className="today-main">
+//         <div className="today-container">
+//           {habits.map((habit, index) => (
+//             <Habit key={index} title={habit.title} options={habit.options} />
+//           ))}
+//         </div>
+//       </section>
+//     </div>
+//   );
+// };
+
+// export default Today;
+
 import React from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_HABIT, EDIT_HABIT, DELETE_HABIT } from '../../utils/mutations';
 import Habit from '../Habit/Habit';
 import Navigation from '../Navigation/Navigation';
-import "./Today.css";
+import './Today.css';
 
-const Today  = () => {
-
+const Today = () => {
   const habits = [
-    { id: 1, title: 'Mental', options: ['Meditate', 'Say Graditudes', 'Practice New Skill', 'Other'] },
+    { id: 1, title: 'Mental', options: ['Meditate', 'Say Gratitude', 'Practice New Skill', 'Other'] },
     { id: 2, title: 'Exercise', options: ['Take A Walk', 'Go to the Gym', 'Dance', 'Other'] },
     { id: 3, title: 'Nutrition', options: ['Drink Water', 'Eat Your Veggies', 'Treat Yourself', 'Other'] },
     { id: 4, title: 'Entertainment', options: ['Read A Chapter', 'Watch A New Movie', 'Listen to a Podcast', 'Other'] }
   ];
+
+  const [addHabit] = useMutation(ADD_HABIT);
+  const [editHabit] = useMutation(EDIT_HABIT);
+  const [deleteHabit] = useMutation(DELETE_HABIT);
 
   return (
     <div>
@@ -18,7 +53,14 @@ const Today  = () => {
       <section className="today-main">
         <div className="today-container">
           {habits.map((habit, index) => (
-            <Habit key={index} title={habit.title} options={habit.options} />
+            <Habit
+              key={index}
+              title={habit.title}
+              options={habit.options}
+              addHabit={addHabit}
+              editHabit={editHabit}
+              deleteHabit={deleteHabit}
+            />
           ))}
         </div>
       </section>
---
+++ client/src/components/Today/placeholder.js
@@ -0,0 +1,121 @@
+// Here is queries.js:
+
+import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      }
+    }
+`;
+
+// Added Habits below:
+
+export const QUERY_ME = gql`
+query me {
+  me {
+    username
+    diaries {
+      diaryText
+      date
+      _id
+    }
+    habits {
+      habitText
+      date
+      _id
+    }
+    _id
+    email
+  }
+}
+`;
+
+// Here is mutations.js:
+
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const ADD_DIARY = gql`
+mutation Mutation($diaryText: String!) {
+  addDiary(diaryText: $diaryText) {
+    username
+    diaries {
+      diaryText
+      date
+      _id
+    }
+  }
+}
+`
+export const EDIT_DIARY = gql`
+mutation Mutation($diaryId: ID!, $diaryText: String!) {
+  editDiary(diaryId: $diaryId, diaryText: $diaryText) {
+      diaryText
+      _id
+  }
+}
+`
+export const DELETE_DIARY = gql`
+mutation Mutation($diaryId: ID!) {
+  deleteDiary(diaryId: $diaryId) {
+      _id
+    }
+}
+`
+
+export const ADD_HABIT = gql`
+mutation Mutation($habitText: String!) {
+  addHabit(habitText: $habitText) {
+    username
+    habits {
+      habitText
+      date
+      _id
+    }
+  }
+}
+`
+export const EDIT_HABIT = gql`
+mutation Mutation($habitId: ID!, $habitText: String!) {
+  editHabit(habitId: $habitId, habitText: $habitText) {
+      habitText
+      _id
+  }
+}
+`
+export const DELETE_HABIT = gql`
+mutation Mutation($habitId: ID!) {
+  deleteHabit(habitId: $habitId) {
+      _id
+    }
+}
+`
